fa36a804f43290732cc4fbf10534a66357dbb52a
segfo <k.segfo@gmail.com>
2021-04-11 15:17:29 +0900

    day4 complete
    ビルドスクリプトの改良
    ビルド設定の修正
    カーネルの呼び出し規約をsysv64に変更

---
+++ build.rs
@@ -0,0 +1,10 @@
+use std::path::Path;
+use std::process::Command;
+use std::{env, error::Error};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let out_dir = env::var("OUT_DIR").unwrap();
+    println!("cargo:rustc-link-search=native={}", out_dir);
+    println!("cargo:rustc-link-lib=static=kernel_common");
+    Ok(())
+}
